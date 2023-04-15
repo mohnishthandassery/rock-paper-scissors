bc9c152239b46e3a7e2d6b3670b888a3f049c2ab
Mohnish Thandassery <mohnishthandasserycode@gmail.com>
2023-04-15 14:36:46 +0530

    update design and add animations

--- main.js
+++ main.js
@@ -1,6 +1,26 @@
 let playerScore = 0;
 let computerScore = 0;
 
+const ROCK_ASSET = "./assets//images/icons8-rock-48.png";
+const PAPER_ASSET = "./assets//images/icons8-paper-48.png";
+const SCISSOR_ASSET = "./assets//images/icons8-scissor-64.png";
+
+const imageMap = {
+  rock: ROCK_ASSET,
+  paper: PAPER_ASSET,
+  scissors: SCISSOR_ASSET,
+};
+
+function createImage(section, choice, divClassName, imageClassName) {
+  const div = document.createElement("div");
+  div.setAttribute("class", divClassName);
+  const image = document.createElement("img");
+  image.setAttribute("src", imageMap[choice]);
+  image.setAttribute("class", imageClassName);
+  div.appendChild(image);
+  section.appendChild(div);
+}
+
 function getRandomIndex(max) {
   return Math.floor(Math.random() * max);
 }
@@ -68,6 +88,10 @@ function game(e) {
   } else {
     updateDOM(result);
   }
+  const resultP = document.getElementById("result-value");
+  resultP.innerHTML = "";
+  createImage(resultP, computerSelection, "computer-selection", "bounceInDown");
+  createImage(resultP, playerSelection, "player-selection", "bounceInUp");
 }
 
 const rps = document.querySelectorAll("button.rps");
